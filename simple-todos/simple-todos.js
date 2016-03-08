e4e1a0d94b6ed5cbe896b10c58893725332b6bd8
“Richard <richgurney1987@gmail.com>
2016-03-08 19:23:46 +0000

    added checkbox and delete button and logic to handle update and delete

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -28,5 +28,16 @@ if (Meteor.isClient) {
       }
     });
 
+  Template.task.events({
+      "click .toggle-checked": function () {
+        // Set the checked property to the opposite of its current value
+        Tasks.update(this._id, {
+          $set: {checked: ! this.checked}
+        });
+      },
+      "click .delete": function () {
+        Tasks.remove(this._id);
+      }
+    });
 }
 
